4d549105516fa66221a543b3079e11f5af8e61cc
hdlmx <hdlmx@outlook.com>
2022-05-06 16:55:45 +0800

    增加服务端口配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue()],
+    server: {
+        port: 3001,//本地前端服务启动端口
+        open: true,//自动打开，
+        base: '/',
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8082',
+                changeOrigin: true,//开启
+                rewrite: (path) => path.replace('/api', '')
+
+            }
+
+        }
+    }
 })
